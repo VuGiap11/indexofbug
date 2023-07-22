13d63bc72d8fc83facb6da3d8dab4a027fdc2a40
vu giap <vudinhgiap11@gmail.com>
2023-07-22 21:13:15 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexoutof
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<char> characters = new List<char>();
+            characters.InsertRange(0, new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+            int value = characters[6];
+            try
+            {
+                List characters = new List();
+                characters.InsertRange(0, new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = characters[6];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
